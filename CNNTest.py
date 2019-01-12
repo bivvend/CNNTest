3209e79bb26328ee0ee7d9dac68fbf9d63402e95
bivvend <simon.j.henley@gmail.com>
2019-01-12 17:28:54 +0000

    first commit

---
+++ CNNTest.py
@@ -0,0 +1,20 @@
+from keras import layers
+from keras import models
+import matplotlib.pyplot as plt
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+if __name__== "__main__":
+    model.summary()
+    (train_images, train_labels) , (test_images, test_labels) = mnist.load_datat()
+    digit = train_images[4]
+    plt.imshow(digit, cmap = plt.c.binary)
+    plot.show() 
