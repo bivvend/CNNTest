76b2a1ea45e1eedf056eb75b5a8bec74a932ab5c
bivvend <simon.j.henley@gmail.com>
2019-01-24 23:43:22 +0000

    Testing angle fail

--- location_train.py
+++ location_train.py
@@ -8,7 +8,7 @@ import numpy as np
 import cv2
 import matplotlib
 import matplotlib.pyplot as plt
-import pandas as pd
+
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras import layers
@@ -26,6 +26,34 @@ POSSIBLE_SHAPES = ["Square","Triangle","Hexagon"]
 IMAGE_DIM_X = 254
 IMAGE_DIM_Y = 254
 
+def data_gen(image_dir, batch_size_in):
+    """
+    Generator to yield batches of two inputs (per sample) with shapes top_dim and 
+    bot_dim along with their labels.
+    """
+    batch_size = batch_size_in
+    image_file_list = os.listdir(image_dir)
+    num_images = len(image_file_list)    
+    while True:        
+        image_batch = []
+        batch_labels = []
+        for i in range(batch_size):
+            # Create random arrays
+            rand = np.random.randint(0, len(image_file_list))
+            full_path = os.path.join(image_dir, image_file_list[rand])
+            img = cv2.imread(full_path, 0)
+            img= cv2.resize(img, (IMAGE_DIM_X, IMAGE_DIM_Y))
+            img = img.reshape(IMAGE_DIM_X*IMAGE_DIM_Y)
+            img= img.astype('float32')/255.0
+            image_batch.append(img)
+            # Set a label
+            splits = image_file_list[rand].split()
+            angle =  float(splits[6].split('.')[0])/30.0
+            xcoord = float(splits[3])/ 254.0
+            ycoord = float(splits[4])/ 254.0
+            batch_labels.append(xcoord)
+
+        yield np.array(image_batch), np.array(batch_labels)
 
 if __name__ == '__main__':
 
@@ -51,34 +79,36 @@ if __name__ == '__main__':
     for file in file_list:
         file_name_list.append(os.path.join(train_dir , file)) 
     
-    #https://medium.com/@vijayabhaskar96/tutorial-on-keras-imagedatagenerator-with-flow-from-dataframe-8bd5776e45c1
-    # df=pd.read_csv(r".\train.csv")
-    # datagen=ImageDataGenerator(rescale=1./255)
-    # train_generator=datagen.flow_from_dataframe(dataframe=df, directory=".\train_imgs", x_col="id", y_col="label", class_mode="categorical", target_size=(32,32), batch_size=32)
     
     #Build convnet
+    # model = models.Sequential()
+    # model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape = (IMAGE_DIM_X, IMAGE_DIM_Y, 3)))
+    # model.add(layers.MaxPooling2D((2, 2)))
+    # model.add(layers.Conv2D(64, (3,3), activation='relu'))
+    # model.add(layers.MaxPooling2D((2, 2)))
+    # model.add(layers.Conv2D(128, (3,3), activation='relu'))
+    # model.add(layers.MaxPooling2D((2, 2)))
+    # model.add(layers.Conv2D(128, (3,3), activation='relu'))
+    # model.add(layers.Flatten())
+    # model.add(layers.Dense(512, activation='relu'))
+    # model.add(layers.Dense(1))  #angle  
+
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape = (IMAGE_DIM_X, IMAGE_DIM_Y, 3)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3,3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(128, (3,3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(128, (3,3), activation='relu'))
-    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation='relu', input_shape = (IMAGE_DIM_X * IMAGE_DIM_Y,)))
     model.add(layers.Dense(512, activation='relu'))
-    model.add(layers.Dense(3))  # x, y, angle  
+    model.add(layers.Dense(1, activation='linear'))   
 
     print(model.summary())
 
     model.compile(loss='mean_squared_error',     #mean_squared_error #categorical_crossentropy
-                optimizer=optimizers.RMSprop(lr=2e-5),
+                optimizer=optimizers.RMSprop(lr=1e-4),
                 metrics=['acc']) 
     
-    # STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
-    # STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
-    # model.fit_generator(generator=train_generator,
-    #                 steps_per_epoch=STEP_SIZE_TRAIN,
-    #                 validation_data=valid_generator,
-    #                 validation_steps=STEP_SIZE_VALID,
-    #                 epochs=10)
+    train_generator = data_gen(train_dir, 100)
+    valid_generator = data_gen(val_dir, 100)
+
+    history = model.fit_generator(generator=train_generator,
+                    steps_per_epoch=100,
+                    validation_data=valid_generator,
+                    validation_steps=50,
+                    epochs=20)
